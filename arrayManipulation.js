af9e8a24f32dab663ea2eb5a53a1230851caa009
death-inspires-me <nana.esseney@gmail.com>
2024-05-18 11:05:42 +0000

    Completed Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -18,3 +18,29 @@ const input = [2, 2, 3, 10, 5];
 const output = processArray(input);
 console.log(output); 
 //Output: [3, 4, 9, 16, 15]
+
+
+//Task 2
+function formatArrayStrings(stringArray, numberArray) {
+    const result = [];
+
+    for (let i = 0; i < stringArray.length; i++) {
+        const str = stringArray[i];
+        const num = numberArray[i];
+
+        if (num % 2 === 0) {
+            result.push(str.toUpperCase());
+        } else {
+            result.push(str.toLowerCase());
+        }
+    }
+
+    return result;
+}
+
+// Example usage:
+const inputStrings = ["We", "Are", "The", "Exception"];
+const inputNumbers = [2, 1, 5, 6];
+const formattedStrings = formatArrayStrings(inputStrings, inputNumbers);
+console.log(formattedStrings); // Output: ["WE", "are", "the", "EXCEPTION"]
+
