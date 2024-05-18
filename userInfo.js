abceadc33e6552520c9fbd0adcfe2427d75a6683
death-inspires-me <nana.esseney@gmail.com>
2024-05-18 11:44:58 +0000

    Completed Task 3

---
+++ userInfo.js
@@ -0,0 +1,26 @@
+function createUserProfiles(originalNames, modifiedNames) {
+    const profiles = [];
+
+    for (let i = 0; i < originalNames.length; i++) {
+        const profile = {
+            originalName: originalNames[i],
+            modifiedName: modifiedNames[i],
+            id: i + 1, // Start counting from 1
+        };
+        profiles.push(profile);
+    }
+
+    return profiles;
+}
+
+// Example:
+const originalNames = ["Gojo", "Sukuna", "Kenjaku"];
+const modifiedNames = ["gojo", "SUKUNA", "kenjaku"];
+const userProfiles = createUserProfiles(originalNames, modifiedNames);
+console.log(userProfiles);
+// Output:
+// [
+//   { originalName: 'Gojo', modifiedName: 'gojo', id: 1 },
+//   { originalName: 'Sukuna', modifiedName: 'SUKUNA', id: 2 },
+//   { originalName: 'Kenjaku', modifiedName: 'kenjaku', id: 3 }
+// ]
